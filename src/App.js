8d07bc814e8b10cecc13ce951fa481cd580f6a08
Daniel <dannyblaze469@gmail.com>
2022-01-05 20:31:31 -0800

    created components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <button>About</button>
+        <button>Projects</button>
+        <button>Contact Info</button>
       </header>
+      <mainCard>
+
+      </mainCard>
+      <div>
+
+      </div>
     </div>
   );
 }
