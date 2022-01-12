0d20c98dbbc25c43f226084c2420e4e4a45eabe5
Daniel <dannyblaze469@gmail.com>
2022-01-11 18:51:25 -0800

    adding carousel

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,30 @@
-import React from "react";
+import 'bootstrap/dist/css/bootstrap.css'
+import { Carousel } from 'react-bootstrap'
+import image1 from '../assets/linkedin.jpg'
+import image2 from '../assets/Git.png'
 
-function Home() {
+
+const Carousell = () => {
     return (
-        <div></div>
-    );
+        <div>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={image1}
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={image2}
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    )
 }
 
-export default Home;
+export default Carousell
