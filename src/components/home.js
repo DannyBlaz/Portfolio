af2c827876e7d61a7ce06079195f30d2605f1e25
Daniel <dannyblaze469@gmail.com>
2022-01-11 19:43:22 -0800

    working on css

--- src/components/home.js
+++ src/components/home.js
@@ -1,30 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import { Carousel } from 'react-bootstrap'
-import image1 from '../assets/linkedin.jpg'
-import image2 from '../assets/Git.png'
+import React from "react";
+import Carousel from"./carousel.js";
 
-
-const Carousell = () => {
+function Home() {
     return (
         <div>
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={image1}
-                        alt="Second slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={image2}
-                        alt="Second slide"
-                    />
-                </Carousel.Item>
-            </Carousel>
+            <Carousel></Carousel>
         </div>
-    )
+    );
 }
 
-export default Carousell
+export default Home;
