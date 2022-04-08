6a4174c77ab37404aafde367e9abc0b0503d664e
Daniel <dannyblaze469@gmail.com>
2022-04-08 12:15:26 -0700

    made picture responsive

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,7 @@ function About() {
         <div>
             <h1>About</h1>
             <p>
-                Software Engineer skilled in troubleshooting and debugging Ruby, Rails, Node, JavaScript, and React. Supportive and enthusiastic team player dedicated to streamlining processes and efficiently resolving problems as shown in my retail experience at Amazon and Lowe's. Passionate about learning new things and staying current with new technologies.
+                Software Engineer skilled in troubleshooting and debugging Ruby, Rails, Node, JavaScript, and React. Supportive and enthusiastic team player dedicated to streamlining processes and efficiently resolving problems as shown in my experience at Everyday Robotics. Passionate about learning new technologies and staying current with new tools and technologies.
             </p>
         </div>
     );
