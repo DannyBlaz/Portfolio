f1c45449364dafd903a01a20d6cedd762ed29382
Daniel <dannyblaze469@gmail.com>
2022-05-07 14:53:39 -0700

    Moved Links tab

--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,14 @@ function Home() {
     return (
         <div>
             <img className="my-pic" src={me}></img>
-            <p> Find My Resume in The Links Tab </p>
+            <p> 
+                Find My Resume in The Links Tab 
+                Wrote Authentication for the User model, User controller, and Sessions controller.
+                Used Rails Active_model_serializer to reduce the number of fetches made.
+                Wrote reusable code in Ruby for the Model and Controller.
+                Completed the MVC frame with JavaScript React components for Views.
+
+            </p>
         </div>
     );
 }
