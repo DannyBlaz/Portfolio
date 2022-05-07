429e442108677e68dd0511261f7f3db205aca75e
Daniel <dannyblaze469@gmail.com>
2022-05-07 14:22:45 -0700

    Making changes to projects tab

--- src/components/home.js
+++ src/components/home.js
@@ -6,7 +6,7 @@ function Home() {
     return (
         <div>
             <img className="my-pic" src={me}></img>
-            <p> He's Highness</p>
+            <p> He's Highness </p>
         </div>
     );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,12 +12,12 @@ function NavBar() {
                         <Nav.Link href="/portfolio/">Home</Nav.Link>
                         <Nav.Link href="#contact-info">Contact Info</Nav.Link>
                         <Nav.Link href="#about-me">About Me</Nav.Link>
-                        <NavDropdown title="Projects" id="dropdown">
+                        <NavDropdown title="Links" id="dropdown">
+                            <NavDropdown.Item href="https://docs.google.com/document/d/16lquF6AVro1paGDsmO8yIfojx5seNtDY/edit?usp=sharing&ouid=101263549633324859001&rtpof=true&sd=true">Resume</NavDropdown.Item>
+                            <NavDropdown.Divider />
                             <NavDropdown.Item href="https://secure-forest-02892.herokuapp.com">FineArt</NavDropdown.Item>
                             <NavDropdown.Item href="https://pure-temple-59698.herokuapp.com">Message Board</NavDropdown.Item>
                             <NavDropdown.Item href="https://dannyblaz.github.io/food-project/">Food</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
